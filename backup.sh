4b848d21aa1fe37284f1db2712856ec028b2a3d0
Quentin Hacourt <quentin.hacourt@gmail.be>
2020-06-15 17:48:47 +0200

    removed useless output

--- backup.sh
+++ backup.sh
@@ -4,13 +4,6 @@ DRIVENAME=$2
 FULLPATH="/media/usb/${FILENAME}.tar.gz"
 PATHTODRIVE="/dev/${DRIVENAME}"
 
-echo "File name:"
-echo $FULLPATH
-echo "drive name:"
-echo $DRIVENAME
-echo "full path to drive:"
-echo $PATHTODRIVE
-
 sudo mount $PATHTODRIVE /media/usb
 
 tar -czvf $FULLPATH ~/
